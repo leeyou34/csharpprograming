705778c9a8430f918054a7c7c29481ccf31549d2
yh.thomas.lee <yh.thomas.lee@gmail.com>
2022-04-01 00:07:00 +0900

    successfully printed hello world

--- csharpexample/ConsoleApp2/ConsoleApp2/Program.cs
+++ csharpexample/ConsoleApp2/ConsoleApp2/Program.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp2
+namespace ConsoleApp2 //콘솔 앱(.NET Framework)로 프로젝트 열었음.
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("Hello World! This prints out from C#!"); // 예제로 배우는 C# 1강 15분:02초
+            Console.ReadKey();
         }
     }
 }
